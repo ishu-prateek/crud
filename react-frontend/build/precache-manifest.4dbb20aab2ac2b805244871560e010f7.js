9833a367d1bf28c635003eca0de83a92d7810f2c
ishuprateek <you@example.com>
2023-04-16 10:35:31 +0530

    commit

---
+++ react-frontend/build/precache-manifest.4dbb20aab2ac2b805244871560e010f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74633bc8f6e25d9b4ab45d8ec8e93a03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f12282710e7a221e0944",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "30431adc588fca249984",
+    "url": "/static/css/main.e2bc9fc6.chunk.css"
+  },
+  {
+    "revision": "f12282710e7a221e0944",
+    "url": "/static/js/2.27d48ebc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.27d48ebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30431adc588fca249984",
+    "url": "/static/js/main.af50fb61.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  }
+]);
